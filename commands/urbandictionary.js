dafd4f379febeeb7ed591a16d6ab0f6a9e46f350
TimDorsman <tim.dorsman@hotmail.com>
2018-09-16 14:53:49 +0200

    Made sure the app doesn't crash when there are no urban dictionary results

--- commands/urbandictionary.js
+++ commands/urbandictionary.js
@@ -1,23 +1,29 @@
 module.exports = {
-    name: "urban",
-    usage: "!urban <term>",
-    description: "Get the definition of a word",
+	name: "urban",
+	usage: "!urban <term>",
+	description: "Get the definition of a word",
 
-    async run(message, args) {
-        const url = `http://api.urbandictionary.com/v0/define?term=${args[0]}`;
+	async run(message, args) {
+		const url = `http://api.urbandictionary.com/v0/define?term=${args[0]}`;
 
-        const XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
-        let xhr = new XMLHttpRequest();
+		const XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
+		let xhr = new XMLHttpRequest();
 
-        xhr.onreadystatechange = () => {
-            if(xhr.readyState == 4 && xhr.status == 200) {
-                let data = JSON.parse(xhr.responseText);
-                let str = data.list[0].definition
-                let definition = str.replace(/[^a-zA-Z ]/g, '');
-                message.channel.send("```"+ definition + "```");
-            }
-        }
-        xhr.open("GET", url, true);
-        xhr.send();
-    }
+		xhr.onreadystatechange = () => {
+			if(xhr.readyState == 4 && xhr.status == 200) {
+				let data = JSON.parse(xhr.responseText);
+				if(data.list.length > 0) {
+					let str = data.list[0].definition
+					let definition = str.replace(/[^a-zA-Z ]/g, '');
+					message.channel.send("```"+ definition + "```");
+				}
+				else {
+					message.channel.send("Sorry we couldn't find a definition for this word");
+				}
+
+			}
+		}
+		xhr.open("GET", url, true);
+		xhr.send();
+	}
 }
