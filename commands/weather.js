be91681fac9ac949d300dc10326460ee910b566d
TimDorsman <tim.dorsman@hotmail.com>
2018-09-17 21:00:52 +0200

    Fixed broken clouds image

--- commands/weather.js
+++ commands/weather.js
@@ -50,9 +50,11 @@ module.exports = {
 					case 'broken clouds':
 						colorEmbed = 8439299;
 						thumbImage = 'http://www.weatherfor.us/static/verbose/weather_mostly_cloudy.png';
+						break;
 					case 'overcast clouds':
 						colorEmbed = 6235521;
 						thumbImage = 'https://banner2.kisspng.com/20180403/wew/kisspng-weather-forecasting-cloud-cover-wind-snow-cloudy-5ac37740150025.823420371522759488086.jpg';
+						break;
 					default:
 						colorEmbed = 16711681;
 						thumbImage = 'http://pluspng.com/img-png/happy-sun-png-no-background-gallery-recent-updates-cartoon-sunhappy-586.png';
