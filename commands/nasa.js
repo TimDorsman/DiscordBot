45d6e4881afa66c42721e83d13464ba2111197b1
Tim Dorsman <31539107+TimDorsman@users.noreply.github.com>
2018-09-26 15:43:36 +0200

    spaces to indent

--- commands/nasa.js
+++ commands/nasa.js
@@ -1,21 +1,21 @@
 module.exports = {
-    name: "nasa",
-    usage: "!nasa <url>",
-    description: "Displays an space image",
+	name: "nasa",
+	usage: "!nasa <url>",
+	description: "Displays an space image",
 
-    async run(message, args) {
-        const url = 'https://api.nasa.gov/planetary/apod?api_key=930bVCNrl5ZUfi2td5O7wodrAssF52RxKjupeIfc';
+	async run(message, args) {
+		const url = 'https://api.nasa.gov/planetary/apod?api_key=930bVCNrl5ZUfi2td5O7wodrAssF52RxKjupeIfc';
 
-        const XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
-        let xhr = new XMLHttpRequest();
+		const XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
+		let xhr = new XMLHttpRequest();
 
-        xhr.onreadystatechange = () => {
-            if(xhr.readyState == 4 && xhr.status == 200) {
-                let data = JSON.parse(xhr.responseText);
-                message.channel.send(``, {files: [`${data.hdurl}`]})
-            }
-        }
-        xhr.open("GET", url, true);
-        xhr.send();
-    }
+		xhr.onreadystatechange = () => {
+			if(xhr.readyState == 4 && xhr.status == 200) {
+				let data = JSON.parse(xhr.responseText);
+				message.channel.send(``, {files: [`${data.hdurl}`]})
+			}
+		}
+		xhr.open("GET", url, true);
+		xhr.send();
+	}
 }
