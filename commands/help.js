a4961094c01ae692be728aced057128c06ee94b4
Tim Dorsman <31539107+TimDorsman@users.noreply.github.com>
2018-10-18 09:10:38 +0200

    Delete help.js

--- commands/help.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-	name: "list",
-	usage: "!list",
-	description: "Shows a list of the commands",
-
-	async run(message, args) {
-		message.channel.send('``` Use !roll to roll a dice \n Use !nasa to get the picture of the day \n Use !date to get the date \n Use !song <link> to listen to a song \n Use !leave to make the bot leave the voicechannel \n Use !urban <term> to get the definition of a word \n Use !greet to be greeted \n Use !gif <term> to show a cool GIF```');
-	}
-};
